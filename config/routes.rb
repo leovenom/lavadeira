c3581d54c4044d26d14e0fda55fef0c2fbcac94e
leovenom <leonardt@oputlook.com>
2020-09-01 20:00:57 +0100

    test all

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :orders do
     resources :items
   end
-  root to:"clients#new"
+  root to:"clients#index"
   resources :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
